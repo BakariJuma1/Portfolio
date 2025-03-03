07050361e13c7ff2a5dd723e32dca385c86b5244
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-03-03 20:15:47 +0300

    harburgermenu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.getElementById
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".navMenu");
+
+  hamburger.addEventListener("click", function () {
+    // Toggle active class on hamburger
+    this.classList.toggle("change");
+    // Toggle active class on nav menu
+    navMenu.classList.toggle("active");
+  });
+
+  // Close menu when clicking on a nav link
+  document.querySelectorAll(".navMenu a").forEach((link) => {
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("change");
+      navMenu.classList.remove("active");
+    });
+  });
+});
